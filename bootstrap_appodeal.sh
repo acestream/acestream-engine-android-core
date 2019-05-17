fa4e9380946b21755c246a08f1ceb65c4e5ed1c9
Anton <apishel@gmail.com>
2019-05-17 08:56:12 +0300

    chore: update appodeal bootstrap script

--- bootstrap_appodeal.sh
+++ bootstrap_appodeal.sh
@@ -25,6 +25,10 @@ rm okhttp-3.7.0.jar
 rm okio-1.12.0.jar
 rm retrofit-2.2.0.jar
 
+# Remove SDKs for which we use newew versions
+rm adcolony-3.3.4.aar
+rm inmobi-7.1.1.jar
+
 # Copy to libs folder
 mkdir -p $LIBS_DIR
 cp ./*.jar $LIBS_DIR
